e176b34072dd20491e34080c810b93b870b15a32
Dawood <dawood.ali.16.virgo@gmail.com>
2019-12-11 00:59:08 +0500

    complete todo list project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './App.css';
 import Header from './Header';
 import List from './List';
 
 class App extends Component {
+  state = {
+    list: [],
+    field: ''
+  };
+
+  changeField = e => {
+    this.setState({ field: e.target.value });
+  };
+  submit = e => {
+    e.preventDefault();
+    const val = { title: this.state.field, isCompleted: false };
+    this.setState(
+      {
+        field: '',
+        list: [...this.state.list, val]
+      },
+      console.log('click')
+    );
+  };
+
+  doneHandler = index => {
+    const list = this.state.list;
+    const mapResult = list.map((val, ind) => {
+      if (ind === index) {
+        val.isCompleted = !val.isCompleted;
+      }
+      return val;
+    });
+    this.setState({ list: mapResult });
+  };
+
+  removeHandler = index => {
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    this.setState({ list });
+  };
+
   render() {
-    state = {
-      list = [],
-      field = ''
-    } 
+    const { list, field } = this.state;
+
     return (
-      
       <div>
-        const {list, field} = this.state;
-        <Header name={'name'} />
-        <input
-          type="text"
-          value={this.state.field}
-          onChange={this.changeField}
-          placeholder="Enter Text"
-        />
-        <button onClick={this.onClick}>Submit </button>
+        <Header name={moment().format('MMMM Do YYYY, HH:mm:ss')} />
+        <form onSubmit={this.submit}>
+          <input
+            type="text"
+            value={field}
+            onChange={this.changeField}
+            placeholder="Enter a TODO"
+          />
+          <button disabled={field ? false : true}>Submit </button>
+        </form>
         <ul></ul>
-        <List />
+        <List
+          list={list}
+          doneHandler={this.doneHandler}
+          removeHandler={this.removeHandler}
+        />
       </div>
     );
   }
--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class List extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const List = ({ list, doneHandler, removeHandler }) => {
+  return (
+    <div>
+      <ul>
+        {list.map((v, i) => (
+          <li style={{ backgroundColor: v.isCompleted ? 'green' : '' }} key={i}>
+            {v.title}{' '}
+            <button
+              onClick={() => {
+                doneHandler(i);
+              }}
+            >
+              Done
+            </button>{' '}
+            <button
+              onClick={() => {
+                removeHandler(i);
+              }}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default List;
