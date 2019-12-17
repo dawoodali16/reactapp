dfe33b9aa2d121778f116df59dad9146a4e34319
Dawood <dawood.ali.16.virgo@gmail.com>
2019-12-18 01:23:44 +0500

    router project

---
+++ src/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h3>DAWOOD</h3>
+      <h3>DAWOOD</h3>
+    </div>
+  );
+};
--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import Home from './Home';
-import Header from './Header';
-import { Footer } from './Footer';
-
+import Main from './Main';
+import { About } from './About';
+import { Contact } from './Contact';
 class App extends Component {
   render() {
     return (
       <Router>
-        <ul style={{ background: '#fff', padding: 20 }}>
-          <span>
-            <Link to="/">HOME | </Link>
-          </span>
-          <span>
-            <Link to="/head">HEAD | </Link>
-          </span>
-          <span>
-            <Link to="/foot">FOOTER</Link>
-          </span>
-        </ul>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/head">
-            <Header />
-          </Route>
-          <Route path="/foot">
-            <Footer />
-          </Route>
-        </Switch>
+        <Main />
+        <About />
+        <Contact />
       </Router>
     );
   }
